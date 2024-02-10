4332d40e71a4f524feb7e76e019657edeff998cf
rohit-rayavarapu <rohitchowdary78@gmail.com>
2024-02-10 19:38:48 +0530

    javascript exercises.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+let sum = num1+num2
+return sum
 };
+console.log(sumAll(5,9))
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+return year%4===0 && (year %100 !== 0 || year%400===0)
 };
 
+console.log(leapYears(1985))
 // Do not edit below this line
 module.exports = leapYears;
